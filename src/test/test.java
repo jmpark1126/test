60d563b70790a8fdab60c3df0fdb14c6868fc5ed
jmpark1126 <jmpark1126@gmail.com>
2024-03-24 00:22:09 +0900

    브런치 작업물 생성

--- src/test/test.java
+++ src/test/test.java
@@ -10,7 +10,8 @@ public class test {
 		System.out.println("사번 브런치 커밋");
 		System.out.println("20240037 재작업 완료");
 		System.out.println("사번 브랜치 히스토리 확인용 작업");
-		System.out.println("메인 작업물");
+		System.out.println("메인 작업물 가져오기 성공!");
+		System.out.println("브런치 작업물");
 
 	}
 
