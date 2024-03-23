8f59841fc8e5d577c5722c39f2095bc19341e405
jmpark1126 <jmpark1126@gmail.com>
2024-03-24 00:38:08 +0900

    재시도

--- src/test/test.java
+++ src/test/test.java
@@ -18,7 +18,7 @@ public class test {
 		
 		System.out.println("오늘 작업 할당량 완료!");
 		
-		System.out.println("sourcetree를 이용한 브런치 커밋 시도");
+		System.out.println("sourcetree를 이용한 브런치 커밋 재시도");
 	}
 
 }
