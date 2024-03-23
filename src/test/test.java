326e425d21269b836b207539d1104a75bbebbfe9
jmpark1126 <jmpark1126@gmail.com>
2024-03-24 00:00:24 +0900

    브런치 작업물

--- src/test/test.java
+++ src/test/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld! - 메인 작업물");
+		System.out.println("HelloWorld! - 브런치 작업물");
 		System.out.println("New add!");
 		System.out.println("20240037 Branch work");
 		System.out.println("사번 브런치 커밋");
