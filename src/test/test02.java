6b4433b706bef357e4955be30a1c221d9f73405a
jmpark1126 <jmpark1126@gmail.com>
2024-04-09 21:03:07 +0900

    feat : 20240409 커밋

--- src/test/test02.java
+++ src/test/test02.java
@@ -3,7 +3,7 @@ package test;
 public class test02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		 TODO Auto-generated method stub
 		System.out.println("main 작업");
 		System.out.println("메인 브런치 작업물");
 		
