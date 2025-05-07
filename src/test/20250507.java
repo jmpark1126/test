018fac03b5d0b37a77ae7287347bb5c5638e430a
jmpark1126 <jmpark1126@gmail.com>
2025-05-07 21:06:21 +0900

    Merge branch 'main' of https://github.com/jmpark1126/test
    # Conflicts:
    #	src/test/20250507.java

--- src/test/20250507.java
+++ src/test/20250507.java
@@ -3,6 +3,6 @@ package test;
 public class main {
     public static void main(String[] args) {
         System.out.println("초기 세팅 완료");
-        System.out.println("main 수정");
+        System.out.println("20250507 브런치 수정");
     }
 }
