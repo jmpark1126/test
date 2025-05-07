d79a4656ec9fdc434b8f2e60e6c3e2d6f3d0034d
jmpark1126 <jmpark1126@gmail.com>
2025-05-07 21:07:04 +0900

    main 브런치 수정

--- src/test/20250507.java
+++ src/test/20250507.java
@@ -4,5 +4,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("초기 세팅 완료");
         System.out.println("20250507 브런치 수정");
+
+        System.out.println("main 브런치 수정");
     }
 }
