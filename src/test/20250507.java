fd4619e29222b7b357abf79c4c79d65765edaec9
jmpark1126 <jmpark1126@gmail.com>
2025-05-07 21:46:16 +0900

    main 브런치에서 수정한 내용 다른 브런치에 가져와보기

--- src/test/20250507.java
+++ src/test/20250507.java
@@ -6,5 +6,9 @@ public class main {
         System.out.println("20250507 브런치 수정");
 
         System.out.println("main 브런치 수정");
+
+        System.out.println("20250507 브런치 수정");
+
+        System.out.println("main 브런치에서 수정한 내용 다른 브런치에 가져와보기");
     }
 }
